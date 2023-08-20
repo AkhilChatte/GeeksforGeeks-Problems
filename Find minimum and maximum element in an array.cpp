eda4adcc110ec6f0b2b7b4984f9e9ea88742e0e4
AkhilChatte <akhilchattes@gmail.com>
2023-08-20 17:00:00 +0530

    Add files via upload

---
+++ Find minimum and maximum element in an array.cpp
@@ -0,0 +1,13 @@
+pair<long long, long long> getMinMax(long long a[], int n) {
+    long long min=a[0];
+    long long max=a[0];
+    for(int i=0;i < n;i++){
+        if(a[i] < min){
+            min=a[i];
+        }
+        if(a[i] > max){
+            max=a[i];
+        }
+    }
+    return {min,max};
+}
